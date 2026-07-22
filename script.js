c03284bc758584f248cdd1bf2a09c9acdaf6a947
IllusionBD <rjmahedi4748@gmail.com>
2026-07-22 11:00:16 +0600

    Focus input when game starts

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const startBtn = document.getElementById("startBtn");
 const gameArea = document.getElementById("gameArea");
+const typingInput = document.getElementById("typingInput");
 
 startBtn.addEventListener("click", function () {
     gameArea.style.display = "block";
     startBtn.style.display = "none";
+    typingInput.focus();
 });
